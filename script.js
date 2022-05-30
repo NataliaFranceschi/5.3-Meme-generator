624fd41166455853d699d0b166df881e2d325508
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-05-30 17:23:57 -0300

    Projeto finalizado

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const botaoFire = document.getElementById('fire');
 const botaoWater = document.getElementById('water');
 const botaoEarth = document.getElementById('earth');
 const div = document.getElementById('meme-image-container');
+const imagens = document.getElementsByTagName('img');
 
 function createText() {
   texto.innerHTML = input.value;
@@ -21,15 +22,22 @@ inputImagem.addEventListener('change', aparecerImagem);
 
 function mudarBorda(event) {
   if (event.target.id === 'fire') {
-    div.style.border = '3px solid rgb(255, 0, 0)';
-}
-  else if (event.target.id === 'water') {
-    div.style.border = '5px solid rgb(0, 0, 255)';
-}  else {
-    div.style.border = '6px solid rgb(0, 128, 0)';
-}
+    div.style.border = '3px dashed rgb(255, 0, 0)';
+  } else if (event.target.id === 'water') {
+    div.style.border = '5px double rgb(0, 0, 255)';
+  } else {
+    div.style.border = '6px groove rgb(0, 128, 0)';
+  }
 }
 
 botaoFire.addEventListener('click', mudarBorda);
 botaoWater.addEventListener('click', mudarBorda);
 botaoEarth.addEventListener('click', mudarBorda);
+
+function usarImagem(event) {
+  imagem.src = event.target.src;
+}
+
+for (let i = 1; i < imagens.length; i += 1) {
+  imagens[i].addEventListener('click', usarImagem);
+}
