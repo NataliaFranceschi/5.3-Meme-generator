7d6eeeaab5c9f33aaa47f5ee9b961b1ce5b0be6d
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-05-30 14:59:50 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const texto = document.getElementById('meme-text');
 const input = document.getElementsByTagName('input')[0];
+const imagem = document.getElementById('meme-image');
+const inputImagem = document.getElementsByTagName('input')[1];
 
 function createText() {
   texto.innerHTML = input.value;
 }
 
 input.addEventListener('keyup', createText);
+
+function loadFile(event) {
+  imagem.src = URL.createObjectURL(event.target.files[0]);
+}
+
+inputImagem.addEventListener('change', loadFile);
